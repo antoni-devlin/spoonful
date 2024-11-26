11ebd8dd3ee22ce8ba91bb3a5007422bef029de3
antoni-devlin <antoni.devlin.gmail.com>
2024-11-26 17:44:59 +0000

    wip

--- server.js
+++ server.js
@@ -7,48 +7,10 @@ const fastify = Fastify({
 })
 
 // All recipes endpoint
-fastify.get('/recipes/all', async function handler (request, reply) {
-  try {
-    const { data: recipes, error } = await supabase
-      .from('recipes')
-      .select();
-  
-    if (error) {
-      throw new Error(`Failed to fetch recipes: ${error.message}`);
-    }
-  
-    return recipes;
-  } catch (err) {
-    console.error('Error fetching recipes:', err);
-    throw err; // or return a custom error object
-  }
+fastify.get('/recipes/:slug', async function handler (request, reply) {
+  const { slug } = request.params;
+  // your code here
 })
-
-// Search recipes endpoint
-fastify.post('/recipes/search', async function handler (request, reply) {
-  const { title } = request.body;
-
-  if (!title) {
-    reply.status(400).send({ error: 'Title is required' });
-    return;
-  }
-
-  try {
-    const { data: search_results, error } = await supabase
-      .from('recipes')
-      .select()
-      .ilike('title', `%${title}%`);
-
-    if (error) {
-      throw new Error(`An error occured: ${error.message}`);
-    }
-
-    return search_results;
-  } catch (err) {
-    console.error('Error searching recipes:', err);
-    reply.status(500).send({ error: 'Failed to search recipes' });
-  }
-});
 
 // Run the server!
 try {
